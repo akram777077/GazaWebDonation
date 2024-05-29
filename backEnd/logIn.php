a8a92eff62d882cbfa3a5d1fedaf195495459397
akram <akram7.dris7@gmail.com>
2024-05-29 03:31:59 +0100

    Operations LINK -> add sign Up link with database to create new account -> errors of username is used and somthing wrong

--- backEnd/logIn.php
+++ backEnd/logIn.php
@@ -14,7 +14,20 @@
             header("Location: ../frontEnd/html/homePage.php");
             exit();
         }
-    } else if (isset($_POST["singUp"])) {
-        
+    } else if (isset($_POST["signUp"])) {
+        $user=new User(true,$_POST["userName"],$_POST["password"],$_POST["email"],$_POST["country"]);
+        $result = $user->save();
+        if($result==-2)
+        {
+            gePageHtml("../frontEnd/html/LoginPage.html","the user name is in the system",false);
+        }
+        else if($result == 0)
+        {
+            gePageHtml("../frontEnd/html/LoginPage.html","somthing wrong ...",false);
+        }
+        else
+        {
+            gePageHtml("../frontEnd/html/LoginPage.html","your account add in the system",false);
+        }
     }
 ?>
